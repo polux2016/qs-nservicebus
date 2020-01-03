dcfce960e188c305350eb61d06f10da6614655d0
Andrii Poliukhovych <andrii.poliukhovych.s@gmail.com>
2020-01-03 10:16:49 +0200

    Add Connection string constant

---
+++ Shared/GlobalConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Shared
+{
+    public static class GlobalConfiguration
+    {
+        public static string SqlDBConnectionString = "Server = .; Database = SamuraiData; "
+                + "User Id = sa; Password=yourStrong(!)Password;";
+
+    }
+}
